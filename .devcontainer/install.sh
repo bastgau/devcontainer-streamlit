4ee0d42392342e48c64eb201f26474b65f4a0707
Bastien Gautier <bastien.gautier@dcube.fr>
2022-12-05 18:53:02 +0100

    :hammer: Remove azure function reference

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -6,20 +6,11 @@ sudo chown vscode /workspaces/app/.venv
 git config --global --add safe.directory /workspaces/app
 git config --global core.autocrlf true
 
-sudo apt-get update
-sudo apt-get install vim -y
-
 python3 -m venv /workspaces/app/.venv
 PATH="/workspaces/app/.venv/bin:$PATH"
 
 source /workspaces/app/.venv/bin/activate
 pip install --upgrade pip
-# pip install keyring artifacts-keyring
-
-# cat <<EOF >> /workspaces/app/.venv/pip.conf
-# [global]
-# extra-index-url=https://pkgs.dev.azure.com/...
-# EOF
 
 pip install -r /workspaces/app/requirements-dev.txt
 pip install -r /workspaces/app/requirements.txt
